b1c35a065ce781daff2385f1cbf953c763edb797
Mozk Taberenai <mozk@me.com>
2024-08-21 11:23:03 +0900

    src/fs.rs: sort functions and add set_permissions, symlink_metadata

--- src/fs.rs
+++ src/fs.rs
@@ -30,31 +30,6 @@ fn echo(op: impl std::fmt::Display) -> Echo {
     crate::echo().sput("fs", BRIGHT_BLACK).sput(op, BOLD_CYAN)
 }
 
-/// Rename a file or directory to a new name, replacing the original file if `to` already exists.
-///
-/// This is a wrapper around [`std::fs::rename`] that echoes the operation to the console.
-///
-/// # Examples
-///
-/// ```no_run
-/// use scriptant::*;
-///
-/// fn main() -> std::io::Result<()> {
-///     fs::rename("old.txt", "new.txt")?;
-///     Ok(())
-/// }
-/// ```
-pub fn rename(from: impl AsRef<Path>, to: impl AsRef<Path>) -> std::io::Result<()> {
-    let from = from.as_ref();
-    let to = to.as_ref();
-    echo("rename")
-        .sput(from.to_string_lossy(), BOLD_UNDERLINE)
-        .put("->")
-        .sput(to.to_string_lossy(), BOLD_UNDERLINE)
-        .end();
-    std::fs::rename(from, to)
-}
-
 /// Copy the contents of one file to another.
 ///
 /// This is a wrapper around [`std::fs::copy`] that echoes the operation to the console.
@@ -78,31 +53,6 @@ pub fn copy(from: impl AsRef<Path>, to: impl AsRef<Path>) -> std::io::Result<u64
         .sput(to.to_string_lossy(), BOLD_UNDERLINE)
         .end();
     std::fs::copy(from, to)
-}
-
-/// Create a new hard link to a file.
-///
-/// This is a wrapper around [`std::fs::hard_link`] that echoes the operation to the console.
-///
-/// # Examples
-///
-/// ```no_run
-/// use scriptant::*;
-///
-/// fn main() -> std::io::Result<()> {
-///     fs::hard_link("old.txt", "new.txt")?;
-///     Ok(())
-/// }
-/// ```
-pub fn hard_link(original: impl AsRef<Path>, link: impl AsRef<Path>) -> std::io::Result<()> {
-    let original = original.as_ref();
-    let link = link.as_ref();
-    echo("hard_link")
-        .sput(original.to_string_lossy(), BOLD_UNDERLINE)
-        .put("->")
-        .sput(link.to_string_lossy(), BOLD_UNDERLINE)
-        .end();
-    std::fs::hard_link(original, link)
 }
 
 /// Create a new, empty directory at the provided path.
@@ -149,6 +99,31 @@ pub fn create_dir_all(path: impl AsRef<Path>) -> std::io::Result<()> {
     std::fs::create_dir_all(path)
 }
 
+/// Create a new hard link to a file.
+///
+/// This is a wrapper around [`std::fs::hard_link`] that echoes the operation to the console.
+///
+/// # Examples
+///
+/// ```no_run
+/// use scriptant::*;
+///
+/// fn main() -> std::io::Result<()> {
+///     fs::hard_link("old.txt", "new.txt")?;
+///     Ok(())
+/// }
+/// ```
+pub fn hard_link(original: impl AsRef<Path>, link: impl AsRef<Path>) -> std::io::Result<()> {
+    let original = original.as_ref();
+    let link = link.as_ref();
+    echo("hard_link")
+        .sput(original.to_string_lossy(), BOLD_UNDERLINE)
+        .put("->")
+        .sput(link.to_string_lossy(), BOLD_UNDERLINE)
+        .end();
+    std::fs::hard_link(original, link)
+}
+
 /// Given a path, query the file system to get information about a file, directory, etc.
 ///
 /// This is a wrapper around [`std::fs::metadata`] that echoes the operation to the console.
@@ -170,6 +145,29 @@ pub fn metadata(path: impl AsRef<Path>) -> std::io::Result<std::fs::Metadata> {
         .sput(path.to_string_lossy(), BOLD_UNDERLINE)
         .end();
     std::fs::metadata(path)
+}
+
+/// Read the entire contents of a file into a bytes vector.
+///
+/// This is a wrapper around [`std::fs::read`] that echoes the operation to the console.
+///
+/// # Examples
+///
+/// ```no_run
+/// use scriptant::*;
+///
+/// fn main() -> std::io::Result<()> {
+///     let contents = fs::read("foo.txt")?;
+///     println!("{:?}", contents);
+///     Ok(())
+/// }
+/// ```
+pub fn read(path: impl AsRef<Path>) -> std::io::Result<Vec<u8>> {
+    let path = path.as_ref();
+    echo("read")
+        .sput(path.to_string_lossy(), BOLD_UNDERLINE)
+        .end();
+    std::fs::read(path)
 }
 
 /// Returns an iterator over the entries within a directory.
@@ -197,29 +195,6 @@ pub fn read_dir(path: impl AsRef<Path>) -> std::io::Result<std::fs::ReadDir> {
     std::fs::read_dir(path)
 }
 
-/// Read the entire contents of a file into a bytes vector.
-///
-/// This is a wrapper around [`std::fs::read`] that echoes the operation to the console.
-///
-/// # Examples
-///
-/// ```no_run
-/// use scriptant::*;
-///
-/// fn main() -> std::io::Result<()> {
-///     let contents = fs::read("foo.txt")?;
-///     println!("{:?}", contents);
-///     Ok(())
-/// }
-/// ```
-pub fn read(path: impl AsRef<Path>) -> std::io::Result<Vec<u8>> {
-    let path = path.as_ref();
-    echo("read")
-        .sput(path.to_string_lossy(), BOLD_UNDERLINE)
-        .end();
-    std::fs::read(path)
-}
-
 /// Read the entire contents of a file into a string.
 ///
 /// This is a wrapper around [`std::fs::read_to_string`] that echoes the operation to the console.
@@ -241,31 +216,6 @@ pub fn read_to_string(path: impl AsRef<Path>) -> std::io::Result<String> {
         .sput(path.to_string_lossy(), BOLD_UNDERLINE)
         .end();
     std::fs::read_to_string(path)
-}
-
-/// Write a slice as the entire contents of a file.
-///
-/// This is a wrapper around [`std::fs::write`] that echoes the operation to the console.
-///
-/// # Examples
-///
-/// ```no_run
-/// use scriptant::*;
-///
-/// fn main() -> std::io::Result<()> {
-///     fs::write("foo.txt", b"Hello, world!")?;
-///     Ok(())
-/// }
-/// ```
-pub fn write(path: impl AsRef<Path>, contents: impl AsRef<[u8]>) -> std::io::Result<()> {
-    let path = path.as_ref();
-    let contents = contents.as_ref();
-    echo("write")
-        .put(format_args!("{} bytes", contents.len()))
-        .put("->")
-        .sput(path.to_string_lossy(), BOLD_UNDERLINE)
-        .end();
-    std::fs::write(path, contents)
 }
 
 /// Removes an empty directory.
@@ -332,4 +282,102 @@ pub fn remove_file(path: impl AsRef<Path>) -> std::io::Result<()> {
         .sput(path.to_string_lossy(), BOLD_UNDERLINE)
         .end();
     std::fs::remove_file(path)
+}
+
+/// Rename a file or directory to a new name, replacing the original file if `to` already exists.
+///
+/// This is a wrapper around [`std::fs::rename`] that echoes the operation to the console.
+///
+/// # Examples
+///
+/// ```no_run
+/// use scriptant::*;
+///
+/// fn main() -> std::io::Result<()> {
+///     fs::rename("old.txt", "new.txt")?;
+///     Ok(())
+/// }
+/// ```
+pub fn rename(from: impl AsRef<Path>, to: impl AsRef<Path>) -> std::io::Result<()> {
+    let from = from.as_ref();
+    let to = to.as_ref();
+    echo("rename")
+        .sput(from.to_string_lossy(), BOLD_UNDERLINE)
+        .put("->")
+        .sput(to.to_string_lossy(), BOLD_UNDERLINE)
+        .end();
+    std::fs::rename(from, to)
+}
+
+/// Changes the permissions found on a file or a directory.
+///
+/// This is a wrapper around [`std::fs::set_permissions`] that echoes the operation to the console.
+///
+/// # Examples
+///
+/// ```no_run
+/// use scriptant::*;
+///
+/// fn main() -> std::io::Result<()> {
+///     let mut perms = std::fs::metadata("foo.txt")?.permissions();
+///     perms.set_readonly(true);
+///     fs::set_permissions("foo.txt", perms)?;
+///     Ok(())
+/// }
+/// ```
+pub fn set_permissions(path: impl AsRef<Path>, perm: std::fs::Permissions) -> std::io::Result<()> {
+    let path = path.as_ref();
+    echo("set_permissions")
+        .sput(path.to_string_lossy(), BOLD_UNDERLINE)
+        .end();
+    // TODO: print the permission bits
+    std::fs::set_permissions(path, perm)
+}
+
+/// Query the metadata about a file without following symlinks.
+///
+/// This is a wrapper around [`std::fs::symlink_metadata`] that echoes the operation to the console.
+///
+/// # Examples
+///
+/// ```no_run
+/// use scriptant::*;
+///
+/// fn main() -> std::io::Result<()> {
+///     let metadata = fs::symlink_metadata("foo.txt")?;
+///     println!("{:?}", metadata.file_type());
+///     Ok(())
+/// }
+/// ```
+pub fn symlink_metadata(path: impl AsRef<Path>) -> std::io::Result<std::fs::Metadata> {
+    let path = path.as_ref();
+    echo("symlink_metadata")
+        .sput(path.to_string_lossy(), BOLD_UNDERLINE)
+        .end();
+    std::fs::symlink_metadata(path)
+}
+
+/// Write a slice as the entire contents of a file.
+///
+/// This is a wrapper around [`std::fs::write`] that echoes the operation to the console.
+///
+/// # Examples
+///
+/// ```no_run
+/// use scriptant::*;
+///
+/// fn main() -> std::io::Result<()> {
+///     fs::write("foo.txt", b"Hello, world!")?;
+///     Ok(())
+/// }
+/// ```
+pub fn write(path: impl AsRef<Path>, contents: impl AsRef<[u8]>) -> std::io::Result<()> {
+    let path = path.as_ref();
+    let contents = contents.as_ref();
+    echo("write")
+        .put(format_args!("{} bytes", contents.len()))
+        .put("->")
+        .sput(path.to_string_lossy(), BOLD_UNDERLINE)
+        .end();
+    std::fs::write(path, contents)
 }
