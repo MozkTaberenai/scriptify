0f5b5d2435a4ab9135bb261e2d34c22b71402ce2
Mozk Taberenai <7039158+MozkTaberenai@users.noreply.github.com>
2025-06-02 00:22:09 +0900

    Make all color constants public and reorganize them
    - Changed all const to pub const for external access
    - Removed unnecessary #[allow(dead_code)] attributes
    - Reorganized colors into logical groups (basic colors, bright colors)
    - Arranged colors in standard ANSI order

--- src/color.rs
+++ src/color.rs
@@ -1,33 +1,21 @@
 use anstyle::{AnsiColor, Color};
 
-// Currently used colors
-pub(crate) const MAGENTA: Option<Color> = Some(Color::Ansi(AnsiColor::Magenta));
-pub(crate) const CYAN: Option<Color> = Some(Color::Ansi(AnsiColor::Cyan));
-pub(crate) const BRIGHT_BLACK: Option<Color> = Some(Color::Ansi(AnsiColor::BrightBlack));
-pub(crate) const BRIGHT_BLUE: Option<Color> = Some(Color::Ansi(AnsiColor::BrightBlue));
+// Basic colors
+pub const BLACK: Option<Color> = Some(Color::Ansi(AnsiColor::Black));
+pub const RED: Option<Color> = Some(Color::Ansi(AnsiColor::Red));
+pub const GREEN: Option<Color> = Some(Color::Ansi(AnsiColor::Green));
+pub const YELLOW: Option<Color> = Some(Color::Ansi(AnsiColor::Yellow));
+pub const BLUE: Option<Color> = Some(Color::Ansi(AnsiColor::Blue));
+pub const MAGENTA: Option<Color> = Some(Color::Ansi(AnsiColor::Magenta));
+pub const CYAN: Option<Color> = Some(Color::Ansi(AnsiColor::Cyan));
+pub const WHITE: Option<Color> = Some(Color::Ansi(AnsiColor::White));
 
-// Additional colors for future use
-#[allow(dead_code)]
-const BLACK: Option<Color> = Some(Color::Ansi(AnsiColor::Black));
-#[allow(dead_code)]
-pub(crate) const RED: Option<Color> = Some(Color::Ansi(AnsiColor::Red));
-#[allow(dead_code)]
-pub(crate) const GREEN: Option<Color> = Some(Color::Ansi(AnsiColor::Green));
-#[allow(dead_code)]
-pub(crate) const YELLOW: Option<Color> = Some(Color::Ansi(AnsiColor::Yellow));
-#[allow(dead_code)]
-pub(crate) const BLUE: Option<Color> = Some(Color::Ansi(AnsiColor::Blue));
-#[allow(dead_code)]
-const WHITE: Option<Color> = Some(Color::Ansi(AnsiColor::White));
-#[allow(dead_code)]
-const BRIGHT_RED: Option<Color> = Some(Color::Ansi(AnsiColor::BrightRed));
-#[allow(dead_code)]
-const BRIGHT_GREEN: Option<Color> = Some(Color::Ansi(AnsiColor::BrightGreen));
-#[allow(dead_code)]
-const BRIGHT_YELLOW: Option<Color> = Some(Color::Ansi(AnsiColor::BrightYellow));
-#[allow(dead_code)]
-const BRIGHT_MAGENTA: Option<Color> = Some(Color::Ansi(AnsiColor::BrightMagenta));
-#[allow(dead_code)]
-const BRIGHT_CYAN: Option<Color> = Some(Color::Ansi(AnsiColor::BrightCyan));
-#[allow(dead_code)]
-const BRIGHT_WHITE: Option<Color> = Some(Color::Ansi(AnsiColor::BrightWhite));
+// Bright colors
+pub const BRIGHT_BLACK: Option<Color> = Some(Color::Ansi(AnsiColor::BrightBlack));
+pub const BRIGHT_RED: Option<Color> = Some(Color::Ansi(AnsiColor::BrightRed));
+pub const BRIGHT_GREEN: Option<Color> = Some(Color::Ansi(AnsiColor::BrightGreen));
+pub const BRIGHT_YELLOW: Option<Color> = Some(Color::Ansi(AnsiColor::BrightYellow));
+pub const BRIGHT_BLUE: Option<Color> = Some(Color::Ansi(AnsiColor::BrightBlue));
+pub const BRIGHT_MAGENTA: Option<Color> = Some(Color::Ansi(AnsiColor::BrightMagenta));
+pub const BRIGHT_CYAN: Option<Color> = Some(Color::Ansi(AnsiColor::BrightCyan));
+pub const BRIGHT_WHITE: Option<Color> = Some(Color::Ansi(AnsiColor::BrightWhite));
